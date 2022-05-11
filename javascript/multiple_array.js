4ecf27ff76432f528bfb2d89f57b1d8035d978f4
Renato Hideki Osaka <renato.osaka@hotmail.com>
2022-05-11 14:09:07 -0300

    Write a program that inserts multiple arrays (array1, array2, array3) into main array (mainArray), then loop mainArray and print each inserted array

---
+++ javascript/multiple_array.js
@@ -0,0 +1,10 @@
+const mainArray = []
+
+mainArray.push([1, 2, 3, 4])
+mainArray.push(['a', 'b', 'c'])
+mainArray.push([0, 9, 8])
+mainArray.push(['i', 'o', 'a', 's', 'y', 's'])
+
+for(const value of mainArray) {
+  console.log(value)
+}
