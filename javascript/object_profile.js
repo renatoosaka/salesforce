46b854a7634bdf5ad94228b4667acbe96da69306
Renato Hideki Osaka <renato.osaka@hotmail.com>
2022-05-11 14:22:39 -0300

    Create object with your profile details and print values using 3 different ways

---
+++ javascript/object_profile.js
@@ -0,0 +1,9 @@
+const profile = {
+  name: 'Renato',
+  project: 'Digital_US | Commerce Cloud',
+  company: 'ioasys + alpargatas'
+}
+
+console.log(profile)
+console.log(profile['name'])
+console.log(profile.company)
