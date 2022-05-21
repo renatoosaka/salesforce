7390ed8cb49af17156198b4770cc7b6606b05aa3
Renato Hideki Osaka <renato.osaka@hotmail.com>
2022-05-21 15:37:25 -0300

    Use a SFCC Script Api to get current basket and display product details on isml

--- cartridges/app_custom_osaka_training/cartridge/controllers/MainController.js
+++ cartridges/app_custom_osaka_training/cartridge/controllers/MainController.js
@@ -3,8 +3,15 @@ const server = require('server');
 server.get('Show', function (req, res, next) {
   const productMgr = require('dw/catalog/ProductMgr');
   const product = productMgr.getProduct('product-1');
-
   render('show', { product: product })
+  next()
+})
+
+server.get('Basket', function(req, res, next) {
+  const basketMgr = require('dw/order/BasketMgr');
+  const basket = basketMgr.getCurrentBasket();
+  const products = basket.getAllProductLineItems();
+  render('basket', { products: products })
   next()
 })
 
